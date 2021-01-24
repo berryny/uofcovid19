0f846763aec1b39e55a3d73c086aee6513cee0d5
berryny <judesire@yahoo.com>
2021-01-24 17:23:37 -0500

    update about

--- src/About.jsx
+++ src/About.jsx
@@ -20,12 +20,15 @@ function About() {
         </figure>
       </Container>
 
-      <Container fluid>
+      {/* <Container fluid>
         <div className="row row-cols-1 row-cols-md-3 g-4">
           <div className="col">
             <div className="card h-100">
               <img src="..." className="card-img-top" alt="..." />
-              <div className="card-body">
+              <div className="card-body
+              
+              
+              ">
                 <h5 className="card-title">Card title</h5>
                 <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
               </div>
@@ -71,7 +74,7 @@ function About() {
             </div>
           </div>
         </div>
-      </Container>
+      </Container> */}
     </div>
   )
 }
