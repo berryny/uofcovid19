728220850c2d83df84b2a46cebb4172c7f83b2de
berryny <judesire@yahoo.com>
2021-01-24 22:56:17 -0500

    update app.py call to react index

--- app.py
+++ app.py
@@ -2,11 +2,11 @@ import db
 from flask import Flask, request, jsonify
 import requests
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='../build', static_url_path='/')
 
 @app.route('/')
 def flask_mongodb_atlas():
-    return "flask mongodb atlas!"
+    return app.send_static_file('index.html')
 
 @app.route("/checkin", methods=['POST', 'GET'])
 def checkin():
