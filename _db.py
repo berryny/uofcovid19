b662b21f358edd74ad01c1e6cac12427522cdda5
berryny <judesire@yahoo.com>
2021-01-28 02:57:29 -0500

    add mern

---
+++ _db.py
@@ -0,0 +1,18 @@
+from flask import Flask
+from flask_pymongo import pymongo
+from app import app
+
+from dotenv import load_dotenv
+
+# py-dotenv 0.1
+# https://pypi.org/project/py-dotenv/
+import os
+from py_dotenv import read_dotenv
+
+dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
+read_dotenv(dotenv_path)
+
+CONNECTION_STRING = os.getenv('MONGODB_URI')
+client = pymongo.MongoClient(CONNECTION_STRING)
+db = client.get_database(os.getenv('MONGODB_DB'))
+user_collection = pymongo.collection.Collection(db, 'user_collection')
