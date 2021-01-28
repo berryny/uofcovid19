ea12474823ce25292058db52b69fbe13dc300256
berryny <judesire@yahoo.com>
2021-01-28 12:34:22 -0500

    remove locahost

--- src/components/create-exercise.component.js
+++ src/components/create-exercise.component.js
@@ -23,7 +23,7 @@ export default class CreateExercise extends Component {
     }
 
     componentDidMount() {
-        axios.get('http://localhost:5000/users/')
+        axios.get('./users/')
             .then(response => {
                 if (response.data.length > 0) {
                     this.setState({
