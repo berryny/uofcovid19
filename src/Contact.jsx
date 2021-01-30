6174d3962d42fbc7e836ed079bb1f2ebd2736288
berryny <judesire@yahoo.com>
2021-01-30 11:52:40 -0500

    update

---
+++ src/Contact.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+
+import { Container } from "react-bootstrap"
+
+function Contact() {
+  return (
+    <div>
+      <Container fluid className="secondary-banner text-center text-white">
+        <h1>Contact Page</h1>
+      </Container>
+    </div>
+  )
+}
+export default Contact;
